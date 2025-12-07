ee11586a5be9f09468003246c72cff6706e64089
leonardohenriquedc <leonardodamascenodev@gmail.com>
2025-12-07 14:15:22 -0400

    updating a struct

--- super_trunfo_game.c
+++ super_trunfo_game.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main() {
+void carta(void) {
   char state, code_card[3], city[20];
   int population, amount_point_turist;
   float area, pib;
 
+  scanf("%c", NULL);
   printf("Digite o nome da cidade: ");
   fgets(city, sizeof(city), stdin);
-
+  scanf("%c", NULL);
   printf("Digite um codigo para a carta (Deve conter pelo menos uma letra e um "
          "numero): ");
   scanf("%s", code_card);
@@ -25,8 +26,21 @@ int main() {
   printf("Digite o PIB da cidade em questão: ");
   scanf("%f", &pib);
 
-  printf("As informações da carta são: \nEstado: %c\nCódigo: %s\nNome da "
-         "Cidade: %s"
-         "Área: %f km²\nPIB: %f\nNúmero de pontos turísticos: %d \n",
+  printf("As informações da carta são:\n"
+         "Estado: %c\n"
+         "Código: %s\n"
+         "Nome da Cidade: %s\n"
+         "Área: %f km²\n"
+         "PIB: %f\n"
+         "Número de pontos turísticos: %d\n",
          state, code_card, city, area, pib, amount_point_turist);
+}
+
+int main() {
+
+  printf("CARTA 1: \n");
+  carta();
+
+  printf("CARTA 2: \n");
+  carta();
 }
